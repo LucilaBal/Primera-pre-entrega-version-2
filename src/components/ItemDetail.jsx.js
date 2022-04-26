8cd99be7d2addb41152ed7767dd1e127bc013ff9
LucilaBal <95441877+LucilaBal@users.noreply.github.com>
2022-04-25 23:22:17 -0300

    Add files via upload

---
+++ src/components/ItemDetail.jsx.js
@@ -0,0 +1,34 @@
+import ItemCount from './ItemCount';
+
+const ItemDetail = ({ item }) => {
+
+    const onAdd = (qty) => {
+        alert("You have selected " + qty + " items.");
+    }
+
+    return (
+        <>
+        {
+            item.image
+            ? 
+            <DetailContainer>
+                <WrapperDetail>
+                    <ImgContainer>
+                        <ImageDetail src={item.image[0]} />
+                    </ImgContainer>
+                    <InfoContainer>
+                        <Title>{item.name}</Title>
+                        <Desc>{item.description}</Desc>
+                        <Price>$ {item.cost}</Price>
+                        <Desc>{item.stock} unidades en stock</Desc>
+                    </InfoContainer>
+                    <ItemCount stock={item.stock} initial={1} onAdd={onAdd} />
+                </WrapperDetail>
+            </DetailContainer>
+            : <p>Cargando...</p>
+        }
+        </>
+    );
+}
+
+export default ItemDetail;
